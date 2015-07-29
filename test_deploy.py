65c516e0b105d6501a2e796ecfa7e8517875fc92
Andrews Medina <andrewsmedina@gmail.com>
2015-07-29 16:24:32 -0300

    create and remove app methods

--- test_deploy.py
+++ test_deploy.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import getpass
+import time
 
 from splinter import Browser
 
@@ -13,7 +14,7 @@ class RollbackTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_oauth_login(self):
+    def oauth_login(self):
         self.browser.visit(self.dashboard)
         self.browser.find_by_css(".btn").click()
 
@@ -26,3 +27,30 @@ class RollbackTest(unittest.TestCase):
         self.browser.fill("email", email)
         self.browser.fill("password", password)
         self.browser.find_by_css(".enabled").click()
+
+    def create_app(self):
+        self.oauth_login()
+
+        url = "{}/apps/create/".format(self.dashboard)
+        self.browser.visit(url)
+
+        pool = os.environ.get("POOL")
+        team = os.environ.get("TEAM")
+        self.browser.fill("name", "tsuru-dashboard-automate-app")
+        self.browser.select("platform", "python")
+        self.browser.select("pool", pool)
+        self.browser.select("teamOwner", team)
+        self.browser.find_by_css(".btn").click()
+        self.browser.is_text_present("App was successfully created")
+
+    def remove_app(self):
+        app_name = "tsuru-dashboard-automate-app"
+
+        url = "{}/apps/{}/settings/".format(self.dashboard, app_name)
+        self.browser.visit(url)
+
+        self.browser.find_by_css(".btn-danger")[0].click()
+
+        time.sleep(1)
+        self.browser.find_by_css(".remove-confirmation").fill(app_name)
+        self.browser.find_by_css(".btn-remove").click()
